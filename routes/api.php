5aa3ac272825fb6fe82e343f5c37ed185755757a
A7medelsaied <121132744+A7medelsaied@users.noreply.github.com>
2024-06-17 23:18:16 +0300

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\PhoneVerificationController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [ApiController::class, 'register']);
+Route::post('/login', [ApiController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/profile', [ApiController::class, 'profile']);
+    Route::get('/logout', [ApiController::class, 'logout']);
+
+    Route::post('/phone/verify', [PhoneVerificationController::class, 'sendVerificationCode']);
+    Route::post('/phone/verify/code', [PhoneVerificationController::class, 'verifyCode']);
+});
